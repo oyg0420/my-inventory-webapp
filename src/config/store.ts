dbcf83beded9cabcf9245f095859176a4d469731
oyg0420 <oyg900420@gmail.com>
2021-07-26 15:16:07 +0900

    feat(): Persist 적용

--- src/config/store.ts
+++ src/config/store.ts
@@ -1,17 +1,31 @@
 import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from 'modules/sagas';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { rootReducer } from 'modules/reducers';
+
 const useDevTool = process.env.NODE_ENV === 'development';
 const sagaMiddleWare = createSagaMiddleware();
 const middleware = [sagaMiddleWare];
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: [],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export default (() => {
   const store = configureStore({
-    reducer: {},
+    reducer: persistedReducer,
     devTools: useDevTool,
     middleware,
   });
   sagaMiddleWare.run(rootSaga);
 
-  return store;
+  const persistor = persistStore(store);
+
+  return { store, persistor };
 })();
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from 'config/store';
+import configStore from 'config/store';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { theme, GlobalStyle } from 'components/styles';
 import Main from 'components/pages/Main';
+import { PersistGate } from 'redux-persist/es/integration/react';
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={configStore.store}>
     <BrowserRouter>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
-        <Main />
+        <PersistGate loading={null} persistor={configStore.persistor}>
+          <Main />
+        </PersistGate>
       </ThemeProvider>
     </BrowserRouter>
   </Provider>,
